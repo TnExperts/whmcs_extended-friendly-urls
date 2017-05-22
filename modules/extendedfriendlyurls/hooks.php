60c0006c7d8f491e705a079213f0e910ab4e685d
John Stray <johnstray@users.noreply.github.com>
2017-05-22 17:18:19 +0930

    Update hooks.php

--- modules/extendedfriendlyurls/hooks.php
+++ modules/extendedfriendlyurls/hooks.php
@@ -11,26 +11,74 @@ use WHMCS\View\Menu\Item as MenuItem;
 
 function extendedfriendlyurls_getVariables() {
     GLOBAL $smarty, $CONFIG;
-  
-    return array(
-        "WEB_ROOT" => $smarty->tpl_vars["WEB_ROOT"]->value;
-        "SEO_URLS" => $CONFIG["SEOFriendlyUrls"] == "on" ? true : false;
-    );
+	
+	# WEB_ROOT #
+	$WEB_ROOT = $smarty->tpl_vars["WEB_ROOT"]->value;
+	if ( substr( $WEB_ROOT, -1 ) != "/" ) { $WEB_ROOT = $WEB_ROOT . "/"; }
+	
+	# SEO_URLS : true / false #
+	$SEO_URLS = $CONFIG["SEOFriendlyUrls"] == "on" ? true : false;
+	
+	# MEMBERS #
+	
+	
+	# SUPPORT #
+	
+	
+    return array( "WEB_ROOT" => $WEB_ROOT, "SEO_URLS" => $SEO_URLS, "MEMBERS"  => $MEMBERS, "SUPPORT"  => $SUPPORT" );
 }
 
 function extendedfriendlyurls_webRootRecursive($menuObject) {
     $vars = extendedfriendlyurls_getVariables();
     if ( $navbarObject->hasChildren ) {
-	foreach ( $navbarObject->getChildren() as $childName => $childObject ) {
-	    $childUri = $child_object->getUri();
-	    if ( !is_null( $childUri ) && stripos( $childUri, $vars["WEB_ROOT"] ) !== 0 ) {
-	        $childObject->setUri( $vars["WEB_ROOT"] . $childUri );
-	    }
-	    extendedfriendlyurls_webRootRecursive( $childObject );
-	}
+		foreach ( $navbarObject->getChildren() as $childName => $childObject ) {
+	    	$childUri = $child_object->getUri();
+	    	if ( !is_null( $childUri ) && stripos( $childUri, $vars["WEB_ROOT"] ) !== 0 ) {
+	        	$childObject->setUri( $vars["WEB_ROOT"] . $childUri );
+	    	}
+	    	extendedfriendlyurls_webRootRecursive( $childObject );
+		}
     } elseif ( !is_null( $itemUri = $navbarObject->getUri() ) ) {
         if ( stripos( $itemUri, $vars["WEB_ROOT"] ) !== 0 ) {
             $navbarObject->setUri( $vars["WEB_ROOT"] . $itemUri );
         }
     }
 }
+
+function extendedfriendlyurls_modifyNavLinks( MenuItem $navigationObject ) {
+    $vars = extendedfriendlyurls_getVariables();
+	$supportUrls = array(
+		'knowledgebase.php' => 'knowledgebase',
+		'announcements.php' => 'announcements',
+		'serverstatus.php' => 'network-status',
+		'supporttickets.php' => 'support-tickets',
+		'submitticket.php' => 'submit-ticket'
+	);
+	
+	foreach ( $navigationObject->getChildren() as $childName => $childObject ) {
+		$childObjectUri = $childObject->getUri();
+		if ( !is_null( $childObjectUri ) ) {
+			if ( stripos( $childObjectUri, "clientarea.php" ) !== false ) {
+				$childObject->setUri( $vars["MEMBERS"] );
+			} elseif ( in_array( $childObjectUri, $supportUrls ) ) {
+				$childObject->setUri( $vars["SUPPORT"] . $supportUrls[$childObjectUri] . "/" );
+			}
+		}
+		if ( $childObject->hasChildren() ) {
+			foreach ( $childObject->getChildren() as $subChildObject ) {
+				$subChildObjectUri = $subChildObject->getUri();
+				if ( ( $pos = stripos( $subChildObjectUri, "clientarea.php?action=" ) ) !== FALSE ) {
+					$subChildObject->setUri( $vars["MEMBERS"] . substr( $subChildObjectUri, $pos+22) . "/" );
+				} elseif ( in_array( $subChildObjectUri, $supportUrls ) ) {
+					$subChildObject->setUri( $vars["SUPPORT"] . $supportUrls[$subChildObjectUri] . "/" );
+				}
+			}
+		}
+	}
+	extendedfriendlyurls_webRootRecursive( $navigationObject );
+}
+
+add_hook( 'ClientAreaPrimaryNavbar', 1, 'extendedfriendlyurls_modifyNavLinks' );
+add_hook( 'ClientAreaSecondaryNavbar', 1, 'extendedfriendlyurls_modifyNavLinks' );
+add_hook( 'ClientAreaPrimarySidebar', 1, 'extendedfriendlyurls_modifyNavLinks' );
+add_hook( 'ClientAreaSecondarySidebar', 1, 'extendedfriendlyurls_modifyNavLinks' );
