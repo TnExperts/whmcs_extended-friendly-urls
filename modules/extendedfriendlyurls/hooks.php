07a548f099ed1af83950bd0b2381d1e513f4c03d
John Stray <johnstray@users.noreply.github.com>
2017-05-22 13:34:53 +0930

    Create hooks.php

---
+++ modules/extendedfriendlyurls/hooks.php
@@ -0,0 +1,8 @@
+<?php if ( !defined( 'WHMCS' ) ) { die( 'You cannot load this file directly!' ); } // Security Check
+/**
+ * Extended Friendly URLs for WHMCS
+ * by John Stray [https://www.johnstray.id.au/
+ * Copyright 2017 (c) All Rights Reserved
+ * Licensed under the GNU General Public License v3.0+
+ */
+
