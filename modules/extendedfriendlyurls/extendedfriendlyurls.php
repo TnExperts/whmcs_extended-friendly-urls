a9ded984afeeb3c853328f1d9694eea75f492dfe
John Stray <johnstray@users.noreply.github.com>
2017-05-22 14:33:54 +0930

    Update extendedfriendlyurls.php

--- modules/extendedfriendlyurls/extendedfriendlyurls.php
+++ modules/extendedfriendlyurls/extendedfriendlyurls.php
@@ -1,4 +1,4 @@
-<?php if ( !defined( 'WHMCS' ) ) { die( 'You cannot load this file directly!' ); } // Security Check
+<?php if (!defined('WHMCS')) { die('You cannot load this file directly!'); } // Security Check
 /**
  * Extended Friendly URLs for WHMCS
  * by John Stray [https://www.johnstray.id.au/
@@ -12,6 +12,52 @@ function extendedfriendlyurls_config() {
         "desscription"  => "Adds an extended array of friendly urls to WHMCS and converts all navigation links to support them.",
         "version"       => "1.0",
         "author"        => "John Stray",
+        "fields" => array(
+            "members_url" => array(
+                "FriendlyName"  => "\"Members\" URL",
+                "Description"   => "A subfolder URL that ClientArea sections fall under, eg.: \"/members/invoices/\" (Leave blank for none)",
+                "Default"       => "members",
+                "Type"          => "text",
+                "Size"          => "32"
+            ),
+            "support_url" => array(
+                "FriendlyName"  => "\"Support\" URL",
+                "Description"   => "A subfolder URL that Support sections fall under, eg.: \"/support/announcements/\" (Leave blank for none)",
+                "Default"       => "support",
+                "Type"          => "text",
+                "Size"          => "32"
+            )
+        )
     );
     return $configarray;
+}
+
+function extendedfriendlyurls_activate() {
+    try {
+        Capsule::connection()->transaction(
+            function ($connectionManager) {
+                $connectionManager->table('tbladdonmodules')->insert([
+                    'module' => 'extendedfriendlyurls',
+                    'setting' => 'version',
+                    'value' => '1.0'
+                ]);
+            }
+        );
+        return array('status' => 'success', 'description' => 'Extended Friendly URLs sucessfully activated!');
+    } catch (\Exception $e) {
+        return array('status' => 'error', 'description' => 'Unable to successfully activate Extended Friendly URLs! ' . $e->getMessage());
+    }
+}
+
+function extendedfriendlyurls_deactivate() {
+    try {
+        Capsule::connection()->transaction(
+            function ($connectionManager) {
+                $connectionManager->table('tbladdonmodules')->where('module','=','extendedfriendlyurls')->delete();
+            }
+        );
+        return array('status' => 'success', 'description' => 'Extended Friendly URLs successfully deactivated!');
+    } catch (\Exception $e) {
+        return array('status' => 'error', 'description' => 'Unable to deactivate Extended Friendly URLs! '. $e->getMessaget());
+    }
 }
